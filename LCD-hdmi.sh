356faf0621f3f9cc78534830cc000336a0c4bbf6
Marcin Marczyk <m.l.marczyk@gmail.com>
2020-09-01 23:26:42 +0200

    running scripts from every location is successful

--- LCD-hdmi.sh
+++ LCD-hdmi.sh
@@ -1,8 +1,2 @@
-#sudo cp -rf ./etc/modules-HDMI  /etc/modules
-#sudo cp -rf ./usr/share/X11/xorg.conf.d/99-fbturbo.conf-HDMI  /usr/share/X11/xorg.conf.d/99-fbturbo.conf
-#sudo cp ./boot/config-normal.txt /boot/config.txt
-#sudo cp ./cmdline.txt /boot/
-#sudo cp ./inittab /etc/
-#sudo cp -rf ./etc/rc.local /etc/rc.local
-sudo cp ./boot/config-hdmi.txt /boot/config.txt
-#sudo reboot
+sudo cp $HOME/LCD-show/boot/config-hdmi.txt /boot/config.txt
+sudo reboot
--- LCD4C-show.sh
+++ LCD4C-show.sh
@@ -1,18 +1,18 @@
-sudo cp -rf ./nes /home/pi/RetroPie/roms/
+sudo cp -rf $HOME/LCD-show/nes $HOME/RetroPie/roms/
 
-sudo dpkg -i -B ./xinput-calibrator_0.7.5-1_armhf.deb
+sudo dpkg -i -B $HOME/LCD-show/xinput-calibrator_0.7.5-1_armhf.deb
 sudo apt-get install cmake -y
-cd ./rpi-fbcp/build/
+cd $HOME/LCD-show/rpi-fbcp/build/
 sudo cmake ..
 sudo make
 sudo install fbcp /usr/local/bin/fbcp
 cd ../../
 
-sudo cp -rf ./etc/rc.local-gpio /etc/rc.local
+sudo cp -rf $HOME/LCD-show/etc/rc.local-gpio /etc/rc.local
 
-sudo cp ./waveshare4c-overlay.dtb /boot/overlays/waveshare4c.dtbo
-sudo cp ./waveshare4c-overlay.dtb /boot/overlays/
+sudo cp $HOME/LCD-show/waveshare4c-overlay.dtb /boot/overlays/waveshare4c.dtbo
+sudo cp $HOME/LCD-show/waveshare4c-overlay.dtb /boot/overlays/
 
-sudo cp ./boot/config-4c.txt /boot/config.txt
+sudo cp $HOME/LCD-show/boot/config-4c.txt /boot/config.txt
 
 sudo reboot
